8b69a769ace39ec9a6d95ef4cadd9d70ae14ceef
Rahultigaya <rahultigaya482@gmail.com>
2026-08-05 11:25:52 +0530

    05-08-2026

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import FoundersDesk from "./components/Founderdesk";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-slate-50/60 text-slate-900">
+    <div className="min-h-screen bg-slate-50 text-slate-900">
       <Navbar />
       <Hero />
        <TopPerformersDark />
